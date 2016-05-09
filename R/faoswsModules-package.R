985fad873be948e08cc1517a8e3167067644f8ac
Sebastian Campbell <sebastian.campbell@fao.org>
2016-05-09 11:53:50 +0200

    Added package documentation

---
+++ R/faoswsModules-package.R
@@ -0,0 +1,15 @@
+#' Tools to create and use Statistical Working System packages.
+#'
+#' The main functions to get one started with the system are the \code{Add...} functions, namely:
+#' \itemize{
+#' \item \code{\link{AddSettings}} - Add sample settings file for a module
+#' \item \code{\link{AddModule}} - Create dummy module to edit
+#' }
+#'
+#' @docType package
+#' @name faoswsModules-package
+#' @aliases faoswsModules
+#' @author Sebastian Campbell
+#'
+
+NULL
